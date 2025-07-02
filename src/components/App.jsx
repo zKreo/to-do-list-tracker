3f07010321fb6e4eff84daf0eee3125151fc83e3
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2025-07-02 18:44:15 +0100

    basic start

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>To-Do List Tracker</h1>
+            <p>Welcome to your personal task manager!</p>
+        </div>
+    );
+}
+
+export default App;
