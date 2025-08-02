898bf8e959c5c3c446281b13d0247fe8d7147067
zKreo <asher.smith@icloud.com>
2025-08-02 03:28:17 +0100

    i did even more stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,10 @@ import ToDoList from "./to-do-list";
 import { useState } from "react";
 
 const App = () => {
-    
-    const [showingToDoList, setShowingToDoList] = useState(true);
 
     return (
         <div>
-            <ToDoList showingToDoList = {showingToDoList} setShowingToDoList = {setShowingToDoList}/>
+            <ToDoList/>
         </div>
     );
 }
--- src/components/to-do-list.jsx
+++ src/components/to-do-list.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import "../styles/todolist.css";
 
-const ToDoList = ({showingToDoList, setShowingToDoList}) => {
+const ToDoList = () => {
 
+    const [showingToDoList, setShowingToDoList] = useState(true);
     const [x, setx] = useState(0);
+    const [text, settext] = useState("");
+    const [array, setarray] = useState(["fuck elliots mummy", "fuck nats mummy"]);
 
     const toggleShowToDoList = () => {
         setShowingToDoList (!showingToDoList)
@@ -20,15 +23,19 @@ const ToDoList = ({showingToDoList, setShowingToDoList}) => {
             {showingToDoList ? 
             <div>
                 <h1>To Do List</h1>
+                <input placeholder = "gayyy" value = {text} onChange = {(e) => {settext (e.target.value)}}></input>
+                <button style = {{marginLeft : `${x}px`}} onClick={() => {setarray ([...array, text])}}>SUBMIT</button>
+                {array.map((item, index) => (
+                    <li>{item}</li>
+                ))}
             </div> : 
             <div>
                 <h1>Not Showing</h1>
-                
             </div>}
                 <p>{x}</p>
             <button style = {showingToDoList ? {backgroundColor:"00ff00"} : {backgroundColor:"ff0000", color:"white"}} id = "coolbutton" onClick={toggleShowToDoList}>Hola Button</button>
         </div>
-    );
+    )
 }
 
 export default ToDoList;
